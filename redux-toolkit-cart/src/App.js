31c1a76f4ad0bada75ebf3fe4f1d272ff5f00b7c
baothi <baothi246@gmail.com>
2023-04-21 17:49:09 +0700

    create redux toolkit cart

---
+++ redux-toolkit-cart/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+
+function App() {
+
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
